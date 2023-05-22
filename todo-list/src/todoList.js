aa370e7614b813f63811671aa2c1dbc5295811e7
Konstantin <105809305+konstantin-90@users.noreply.github.com>
2023-05-22 12:33:00 +0300

    complite task done/not done

--- todo-list/src/todoList.js
+++ todo-list/src/todoList.js
@@ -1,26 +1,40 @@
-const TodoList = ({ list, remove }) => {
-  return (
-    <>
-      {list.length > 0 ? (
-        <ul className="todo-list">
-          {list.map((entry, index) => (
-            <div className="todo">
-              <li key = {index}> {entry} </li>
+import React, { useState } from 'react';
 
-              <button className="delete-button" onClick={() => {
-                  remove(entry);
-                }}>
-                Delete
-              </button>
-            </div>
-          ))}
-        </ul>
-      ) : (
-        <div className="empty">
-          <p>No task found</p>
-        </div>
-      )}
-    </>
+const TodoList = ({ list, remove }) => {
+  const [taskStatus, setTaskStatus] = useState(Array(list.length).fill('not-done'));
+
+  const toggleTaskStatus = (index) => {
+    const updatedStatus = [...taskStatus];
+    updatedStatus[index] = updatedStatus[index] === 'not-done' ? 'done' : 'not-done';
+    setTaskStatus(updatedStatus);
+  };
+
+  if (list.length > 0) {
+    return (
+      <ul className="todo-list">
+        {list.map((entry, index) => (
+          <div className="todo" key={index}>
+            <li
+              className={taskStatus[index]}
+              key={index}
+              onClick={() => toggleTaskStatus(index)}
+            >
+              {entry}
+            </li>
+
+            <button className="delete-button" onClick={() => remove(entry)}>
+              Delete
+            </button>
+          </div>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <div className="empty">
+      <p>No tasks found</p>
+    </div>
   );
 };
 
